31b1a9446101b3ebff933860bfa71e40d0090380
r4igen <jluispuchuri@gmail.com>
2020-11-21 15:54:35 -0500

    Agregar archivos de proyecto.

---
+++ VisorDbf/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VisorDbf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.lvArchivos = new System.Windows.Forms.ListView();
+            this.btnBuscarRuta = new System.Windows.Forms.Button();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(258, 54);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.Size = new System.Drawing.Size(970, 377);
+            this.dgvDatos.TabIndex = 9;
+            // 
+            // lvArchivos
+            // 
+            this.lvArchivos.HideSelection = false;
+            this.lvArchivos.Location = new System.Drawing.Point(23, 54);
+            this.lvArchivos.Name = "lvArchivos";
+            this.lvArchivos.Size = new System.Drawing.Size(217, 377);
+            this.lvArchivos.TabIndex = 8;
+            this.lvArchivos.UseCompatibleStateImageBehavior = false;
+            this.lvArchivos.View = System.Windows.Forms.View.List;
+            this.lvArchivos.SelectedIndexChanged += new System.EventHandler(this.lvArchivos_SelectedIndexChanged);
+            // 
+            // btnBuscarRuta
+            // 
+            this.btnBuscarRuta.Location = new System.Drawing.Point(1183, 12);
+            this.btnBuscarRuta.Name = "btnBuscarRuta";
+            this.btnBuscarRuta.Size = new System.Drawing.Size(43, 23);
+            this.btnBuscarRuta.TabIndex = 7;
+            this.btnBuscarRuta.Text = "...";
+            this.btnBuscarRuta.UseVisualStyleBackColor = true;
+            this.btnBuscarRuta.Click += new System.EventHandler(this.btnBuscarRuta_Click);
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(20, 17);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(30, 13);
+            this.lblRuta.TabIndex = 6;
+            this.lblRuta.Text = "Ruta";
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(56, 13);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(1121, 20);
+            this.txtRuta.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 442);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.lvArchivos);
+            this.Controls.Add(this.btnBuscarRuta);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.txtRuta);
+            this.Name = "Form1";
+            this.Text = "Visor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.ListView lvArchivos;
+        private System.Windows.Forms.Button btnBuscarRuta;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.TextBox txtRuta;
+    }
+}
+
---
+++ VisorDbf/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+namespace VisorDbf
+{
+    public partial class Form1 : Form
+    {
+        string _cadenaConexionDBF = string.Empty;
+        StringBuilder query;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarRuta_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog oFbd = new FolderBrowserDialog())
+            {
+                if (oFbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtRuta.Text = oFbd.SelectedPath;
+                    DirectoryInfo dir = new DirectoryInfo(txtRuta.Text);
+                    _cadenaConexionDBF = string.Format("provider=Microsoft.jet.oledb.4.0;data source={0};extended properties=dbase III", txtRuta.Text);
+                    foreach (var file in dir.GetFiles())
+                    {
+                        lvArchivos.Items.Add(file.ToString());
+                    }
+                }
+            }
+        }
+
+        private void lvArchivos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection items = this.lvArchivos.SelectedItems;
+            query = new StringBuilder();
+
+            if (items.Count > 0)
+            {
+                query.AppendFormat("Select top 100000 * from {0} ", items[0].Text);
+                //OleDbDataReader dr;
+                DataTable dt = new DataTable();
+                OleDbConnection cn = new OleDbConnection(_cadenaConexionDBF);
+                OleDbCommand cmd = new OleDbCommand(query.ToString(), cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                OleDbDataAdapter oda = new OleDbDataAdapter(query.ToString(), cn);
+                oda.Fill(dt);
+                cn.Close();
+                dgvDatos.DataSource = dt;
+                //dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);                               
+            }
+        }
+    }
+}
